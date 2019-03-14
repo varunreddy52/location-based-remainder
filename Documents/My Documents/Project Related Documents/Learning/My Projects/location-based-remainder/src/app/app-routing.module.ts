ac3782160d4ef7136485531067395d5bda395db4
Varun Reddy <VarunReddy52@gmail.com>
2019-03-14 00:47:17 -0400

    initial commit

---
+++ Documents/My Documents/Project Related Documents/Learning/My Projects/location-based-remainder/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
